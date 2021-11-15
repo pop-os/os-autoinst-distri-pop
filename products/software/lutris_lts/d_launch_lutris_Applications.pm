d1f55868c6e6e0019fdf0c583ad9d32f5188ca7b
bflanagin <bflanagin@pop-os.localdomain>
2021-11-15 08:34:55 -0700

    first round of new software tests lutris,pop_shop,etc.

---
+++ products/software/lutris_lts/d_launch_lutris_Applications.pm
@@ -0,0 +1,38 @@
+# Copyright (C) 2014-2018 SUSE LLC
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, see <http://www.gnu.org/licenses/>.
+
+use base 'basetest';
+use strict;
+use testapi;
+use lib '/var/lib/openqa/tests/pop/';
+#use helpers::displays;
+
+
+sub run {
+
+send_key 'super';
+type_string 'lutris';
+send_key 'ret';
+assert_screen "lutris",1800;
+sleep(20);
+send_key 'super-t';
+assert_screen 'terminal';
+type_string "killall -9 lutris\n";
+type_string "exit\n";
+assert_screen 'desktop';
+
+}
+
+1;
